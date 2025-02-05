c8ee3875eb73be086d3493831419e0f4936ce0eb
lexis-webb <lexiswebb.design@gmail.com>
2025-02-05 03:17:42 -0500

    added photos tom menu items/updatedAPI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection';
+import Menu from '../components/Menu';
 
 const Home = () => {
   return (
+    <>
     <HeroSection />
+    <Menu />
+    </>
 )};
 
 export default Home
