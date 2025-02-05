536e21890a9c43472446493df7ae8dedefb2f290
lexis-webb <lexiswebb.design@gmail.com>
2025-02-05 00:47:14 -0500

    added navbar

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 
 const HeroSection = () => {
     return (
       <section className="heroSection" id="heroSection">
+        <Navbar />
         <div className="container">
           <div className="banner">
             <div className="largeBox">
