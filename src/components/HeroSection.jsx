3b9e2aba2dbb74957e94876d9dd9fd9892769dd1
lexis-webb <lexiswebb.design@gmail.com>
2025-02-04 23:02:13 -0500

    added banner

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section className='heroSection' id="heroSection">
+        <div className="container">
+            <div className="banner">
+                <div className='largeBox'>
+                    <h1 className='title'>Tasty</h1>
+                    <div className="combined_boxes">
+                        <div className="imageBox">
+                            <img src="/mock_logo.jpg" alt="mock logo" />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+  )
+}
+
+export default HeroSection
